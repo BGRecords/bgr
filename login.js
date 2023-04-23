2dfea63c390970b12f0fec62c019d24bf8db8a73
Bossgamerteam <78909975+Bossgamerteam@users.noreply.github.com>
2023-04-23 18:22:48 +0100

    Create login.js

---
+++ login.js
@@ -0,0 +1,46 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVduEn74v20V_WkrlH3vvFAXR6L6_vIYo",
+  authDomain: "bgr-login.firebaseapp.com",
+  projectId: "bgr-login",
+  storageBucket: "bgr-login.appspot.com",
+  messagingSenderId: "740972691618",
+  appId: "1:740972691618:web:00836c59f86b52ea7317fb",
+  measurementId: "G-RYKPY0QHLY"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+const submitButton = document.getElementById("submit");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const main = document.getElementById("main");
+
+
+const returnBtn = document.getElementById("return-btn");
+
+var email, password;
+
+
+submitButton.addEventListener("click", function() {
+  email = emailInput.value;
+  password = passwordInput.value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      window.location.replace('account#' + email + '#success')
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      window.alert("Error occurred. Try again.");
+    });
+});
