37dc3877f66687c4c3a428734ec4a191c0422996
Bossgamerteam <78909975+Bossgamerteam@users.noreply.github.com>
2023-07-01 20:53:28 +0100

    Create WARN.js

---
+++ WARN.js
@@ -0,0 +1,2 @@
+  console.log("%cSTOP!", "background-color: gray; font-size: 90px; color: red;");
+  console.log("%cEntering anything here may give others your account details.", "background-color: gray; font-size: 27px; color: purple;");
